12269953d1e5916e94ccc4d3ba48bad40c1639fd
pratik8696 <mepratikanand@gmail.com>
2021-08-27 14:04:26 +0530

    frontend done1

--- src/components/Partials/Inputgroup.js
+++ src/components/Partials/Inputgroup.js
@@ -13,11 +13,11 @@ function Inputgroup() {
                     title="All Categories"
                     id="input-group-dropdown-1"
                 >
-                    <Dropdown.Item href="#">Action</Dropdown.Item>
-                    <Dropdown.Item href="#">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#">Something else here</Dropdown.Item>
+                    <Dropdown.Item >Action</Dropdown.Item>
+                    <Dropdown.Item >Another action</Dropdown.Item>
+                    <Dropdown.Item >Something else here</Dropdown.Item>
                     <Dropdown.Divider />
-                    <Dropdown.Item href="#">Separated link</Dropdown.Item>
+                    <Dropdown.Item >Separated link</Dropdown.Item>
                 </DropdownButton>
                 <FormControl aria-label="Text input with dropdown button" />
             </InputGroup>
